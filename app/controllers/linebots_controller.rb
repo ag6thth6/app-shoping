7da4abf141e395211b610b3757f54ca52a01e50b
ag6thth6 <51393400+ag6thth6@users.noreply.github.com>
2019-06-05 17:34:57 +0900

    Update linebots_controller.rb

--- app/controllers/linebots_controller.rb
+++ app/controllers/linebots_controller.rb
@@ -18,9 +18,15 @@ class LinebotsController < ApplicationController
         when Line::Bot::Event::MessageType::Text
           # 入力した文字をinputに格納
           input = event.message['text']
-          # search_and_create_messageメソッド内で、楽天APIを用いた商品検索、メッセージの作成を行う
-          message = search_and_create_message(input)
-          client.reply_message(event['replyToken'], message)
+          case input
+          when 'こんにちは'
+          	message = { type: 'text', text: "こんにちは！"}
+          else
+          	client.reply_message(event['replyToken'], message)
+	        # search_and_create_messageメソッド内で、楽天APIを用いた商品検索、メッセージの作成を行う
+    	    message = search_and_create_message(input)
+        	client.reply_message(event['replyToken'], message)
+          end
         end
       end
     end
