1ef39987cd58a1578fe8c830aa60dd9100bc44d3
ag6thth6 <51393400+ag6thth6@users.noreply.github.com>
2019-06-07 14:04:03 +0900

    Update linebots_controller.rb

--- app/controllers/linebots_controller.rb
+++ app/controllers/linebots_controller.rb
@@ -100,6 +100,39 @@ end
 	doc = REXML::Document.new(xml)
 	{
 	  "type": "template",
+	  "altText": "this is a image carousel template",
+	  "template": {
+	      "type": "image_carousel",
+	      "columns": [
+		  {
+		    "imageUrl": "https://example.com/bot/images/item1.jpg",
+		    "action": {
+		      "type": "postback",
+		      "label": "Buy",
+		      "data": "action=buy&itemid=111"
+		    }
+		  },
+		  {
+		    "imageUrl": "https://example.com/bot/images/item2.jpg",
+		    "action": {
+		      "type": "message",
+		      "label": "Yes",
+		      "text": "yes"
+		    }
+		  },
+		  {
+		    "imageUrl": "https://example.com/bot/images/item3.jpg",
+		    "action": {
+		      "type": "uri",
+		      "label": "View detail",
+		      "uri": "http://example.com/page/222"
+		    }
+		  }
+	      ]
+	  }
+	}
+=bigin	{
+	  "type": "template",
 	  "altText": "this is a carousel template",
 	  "template": {
 	      "type": "carousel",
@@ -115,7 +148,7 @@ end
 	      "imageAspectRatio": "rectangle",
 	      "imageSize": "cover"
 	  }
-	}
+	} =end
   end
 
   def create_weatheritem(doc,xpath,i)
