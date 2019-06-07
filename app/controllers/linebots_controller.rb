bf6b613f47d663d00b5ebdd37510811d03d4759e
ag6thth6 <51393400+ag6thth6@users.noreply.github.com>
2019-06-07 15:37:05 +0900

    Update linebots_controller.rb

--- app/controllers/linebots_controller.rb
+++ app/controllers/linebots_controller.rb
@@ -133,7 +133,7 @@ end
 		"thumbnailImageUrl": img,
 		"imageBackgroundColor": "#FFFFFF",
 		"title": input + date,
-		"text": weather + "やで。" + "\n" + "最高気温：" + max + "\n" + "最低気温：" + min + "\n" + "00-06時" + per00to06 + "%" +"\n" + "06-12時" + per06to12 + "%" +"\n" + "12-18時" + per12to18 + "%" +"\n" + "18-24時" + per18to24 + "%",
+		"text": weather + "やで。\n最高気温：" + max + "度\n最低気温：" + min + "度\n降水確率\n00-06時" + per00to06 + "%\n06-12時" + per06to12 + "%\n12-18時" + per12to18 + "%\n18-24時" + per18to24 + "%",
 		"actions": [
 			{
 			    "type": "uri",
