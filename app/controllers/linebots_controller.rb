a2398001479cc799844908a9109edcdd71482c5a
ag6thth6 <51393400+ag6thth6@users.noreply.github.com>
2019-06-07 13:55:55 +0900

    Update linebots_controller.rb

--- app/controllers/linebots_controller.rb
+++ app/controllers/linebots_controller.rb
@@ -98,56 +98,19 @@ end
 	end
 	xml = Net::HTTP.get(uri)
 	doc = REXML::Document.new(xml)
-{
-  "type": "template",
-  "altText": "this is a carousel template",
-  "template": {
-      "type": "carousel",
-      "columns": [
-          {
-            "thumbnailImageUrl": "https://example.com/bot/images/item1.jpg",
-            "imageBackgroundColor": "#FFFFFF",
-            "title": "this is menu",
-            "text": "description",
-	  "actions": [
-		{
-		  "type": "uri",
-		  "label": "詳細",
-		  "uri": "https://hyouhikaku.com"
-		}
-	   ]
-          }
-      ],
-      "imageAspectRatio": "rectangle",
-      "imageSize": "cover"
-  }
-}
-=bigin	{
+	{
 	  "type": "template",
 	  "altText": "this is a carousel template",
 	  "template": {
 	      "type": "carousel",
 	      "columns": [
-		{
-			  #"thumbnailImageUrl": img,
-			  "imageBackgroundColor": "#FFFFFF",
-			  "title": "title",
-			  "text": "text",
-			  "actions": [
-				{
-				  "type": "uri",
-				  "label": "詳細",
-				  "uri": "https://hyouhikaku.com"
-				}
-			   ]
-		}
-		#      create_weatheritem(doc,xpath,1),
-		#     create_weatheritem(doc,xpath,2),
-		#      create_weatheritem(doc,xpath,3),
-		#      create_weatheritem(doc,xpath,4),
-		#      create_weatheritem(doc,xpath,5),
-		 #     create_weatheritem(doc,xpath,6),
-		  #    create_weatheritem(doc,xpath,7)
+		      create_weatheritem(doc,xpath,1),
+		      create_weatheritem(doc,xpath,2),
+		      create_weatheritem(doc,xpath,3),
+		      create_weatheritem(doc,xpath,4),
+		      create_weatheritem(doc,xpath,5),
+		      create_weatheritem(doc,xpath,6),
+		     create_weatheritem(doc,xpath,7)
 	      ],
 	      "imageAspectRatio": "rectangle",
 	      "imageSize": "cover"
@@ -156,21 +119,28 @@ end
   end
 
   def create_weatheritem(doc,xpath,i)
-	#date = doc.elements[xpath + '/info[i]'].attributes["date"]
-	#weather = doc.elements[xpath + '/info[i]/weather'].text # 天気（例：「晴れ」）
-	#img = doc.elements[xpath + '/info[i]/img'].text # 天気（例：「晴れ」）
-	#img = img.sub(/http/,"https")
-	#max = doc.elements[xpath + '/info[1]/temperature/range[1]'].text # 最高気温
-	#min = doc.elements[xpath + '/info[1]/temperature/range[2]'].text # 最低気温
-	#per00to06 = doc.elements[xpath + '/info[i]/rainfallchance/period[1]'].text # 0-6時の降水確率
-	#per06to12 = doc.elements[xpath + '/info[i]/rainfallchance/period[2]'].text # 6-12時の降水確率
-	#per12to18 = doc.elements[xpath + '/info[i]/rainfallchance/period[3]'].text # 12-18時の降水確率
-	#per18to24 = doc.elements[xpath + '/info[i]/rainfallchance/period[4]'].text # 18-24時の降水確率
+	date = doc.elements[xpath + '/info[i]'].attributes["date"]
+	weather = doc.elements[xpath + '/info[i]/weather'].text # 天気（例：「晴れ」）
+	img = doc.elements[xpath + '/info[i]/img'].text # 天気（例：「晴れ」）
+	img = img.sub(/http/,"https")
+	max = doc.elements[xpath + '/info[1]/temperature/range[1]'].text # 最高気温
+	min = doc.elements[xpath + '/info[1]/temperature/range[2]'].text # 最低気温
+	per00to06 = doc.elements[xpath + '/info[i]/rainfallchance/period[1]'].text # 0-6時の降水確率
+	per06to12 = doc.elements[xpath + '/info[i]/rainfallchance/period[2]'].text # 6-12時の降水確率
+	per12to18 = doc.elements[xpath + '/info[i]/rainfallchance/period[3]'].text # 12-18時の降水確率
+	per18to24 = doc.elements[xpath + '/info[i]/rainfallchance/period[4]'].text # 18-24時の降水確率
 	{
-	  #"thumbnailImageUrl": img,
+	  "thumbnailImageUrl": img,
 	  "imageBackgroundColor": "#FFFFFF",
 	  "title": "title",
 	  "text": "text",
+	  "actions": [
+		{
+		  "type": "uri",
+		  "label": "詳細",
+		  "uri": "https://hyouhikaku.com"
+		}
+	   ]
 	}
   end
 	
